abc7b56830b77fd44479332883010310f5c4ee5e
MaximilianV <maximilian-voelker@t-online.de>
2018-11-27 10:41:37 +0100

    Link roles library to choreography contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+const Roles = artifacts.require("./Roles.sol");
 const Choreography = artifacts.require("./Choreography.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Roles);
+  deployer.link(Roles, Choreography);
   deployer.deploy(Choreography);
 };
