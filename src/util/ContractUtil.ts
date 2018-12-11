b8d88841be66afa55350454e6581616b79211e31
Christian Friedow <christian@friedow.com>
2018-12-11 17:16:51 +0100

    Refactored mapping of events to messages

--- src/util/ContractUtil.ts
+++ src/util/ContractUtil.ts
@@ -1,11 +1,18 @@
 import * as TruffleContract from "truffle-contract";
 import * as Web3 from "web3";
 
-import IChoreography, { States } from "@/contract-interfaces/IChoreography";
 import { IMessageHistoryEntry } from "@/components/MessageHistory";
+import IChoreography, { States } from "@/contract-interfaces/IChoreography";
 import User from "./User";
 
 const ChoreographyContract = TruffleContract(require("../../build/contracts/Choreography.json"));
+
+export type EventTypeMapFunction = (contract: IChoreography, logEvent: any) => Promise<IMessageHistoryEntry>;
+
+export interface IEventType {
+  eventType: any;
+  mapFunction: EventTypeMapFunction;
+}
 
 export default class ContractUtil {
 
@@ -36,21 +43,54 @@ export default class ContractUtil {
     });
   }
 
-  public static async getMessageHistory(contract: IChoreography): Promise<IMessageHistoryEntry []> {
-    const LogNewChangeEvent = contract.LogNewChange({}, { fromBlock: 0, toBlock: "latest" });
-    const events = await ContractUtil.getEvents(LogNewChangeEvent);
-    console.log(events);
-    const messages = (await Promise.all(events.map(async (result): Promise<IMessageHistoryEntry> => {
-      const proposerUsername = await contract.getModelerUsername(result.args._proposer);
-      const proposer = await User.build(result.args._proposer, proposerUsername);
-      return {
-        user: proposer,
-        message: "proposed this change",
-        timestamp: new Date(),
-      };
-    })) as any);
-    console.log(messages);
-    return messages;
+  public static async getMessageHistory(contract: IChoreography): Promise<IMessageHistoryEntry[]> {
+    const blockFilter = { fromBlock: 0, toBlock: "latest" };
+    const eventTypes: IEventType[] = [
+      {
+        eventType: contract.LogNewChange({}, blockFilter),
+        mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogVerificationStarted({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogVerificationDone({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogReviewStarted({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogReviewGiven({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogVoteDistribution({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogReviewDone({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+      // {
+      //   eventType: contract.LogProposalProcessed({}, blockFilter),
+      //   mapFunction: ContractUtil.transformLogNewChange},
+    ];
+    const events = await Promise.all(eventTypes.map((eventType) => ContractUtil.getLogEvents(contract, eventType)));
+    // Concat array of arrays
+    return [].concat.apply([], events);
+  }
+
+  public static async getLogEvents(contract: IChoreography, eventType: IEventType): Promise<IMessageHistoryEntry[]> {
+    const events = await ContractUtil.getEvents(eventType.eventType);
+    const messages = await Promise.all(events.map((logEvent) => eventType.mapFunction(contract, logEvent)));
+    return messages as any;
+  }
+
+  public static async transformLogNewChange(contract: IChoreography, logEvent: any):
+    Promise<IMessageHistoryEntry> {
+    const proposerUsername = await contract.getModelerUsername(logEvent.args._proposer);
+    const proposer = await User.build(logEvent.args._proposer, proposerUsername);
+    return {
+      user: proposer,
+      message: "proposed this change",
+      timestamp: new Date(logEvent.args._timestamp * 1000),
+    };
   }
 
 }
