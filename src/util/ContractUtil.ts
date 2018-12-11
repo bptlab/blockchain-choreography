becf0bb6f62fbc456f91a835d03fbf753056eca7
Christian Friedow <christian@friedow.com>
2018-12-11 16:02:37 +0100

    Added first message type parser for message history

--- src/util/ContractUtil.ts
+++ src/util/ContractUtil.ts
@@ -2,6 +2,8 @@ import * as TruffleContract from "truffle-contract";
 import * as Web3 from "web3";
 
 import IChoreography, { States } from "@/contract-interfaces/IChoreography";
+import { IMessageHistoryEntry } from "@/components/MessageHistory";
+import User from "./User";
 
 const ChoreographyContract = TruffleContract(require("../../build/contracts/Choreography.json"));
 
@@ -24,6 +26,31 @@ export default class ContractUtil {
 
   public static async getContractState(contract: IChoreography): Promise<States> {
     return (await contract.state()).toNumber();
+  }
+
+  public static getEvents(eventType: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      eventType.get((error, result) => {
+        resolve(result);
+      });
+    });
+  }
+
+  public static async getMessageHistory(contract: IChoreography): Promise<IMessageHistoryEntry []> {
+    const LogNewChangeEvent = contract.LogNewChange({}, { fromBlock: 0, toBlock: "latest" });
+    const events = await ContractUtil.getEvents(LogNewChangeEvent);
+    console.log(events);
+    const messages = (await Promise.all(events.map(async (result): Promise<IMessageHistoryEntry> => {
+      const proposerUsername = await contract.getModelerUsername(result.args._proposer);
+      const proposer = await User.build(result.args._proposer, proposerUsername);
+      return {
+        user: proposer,
+        message: "proposed this change",
+        timestamp: new Date(),
+      };
+    })) as any);
+    console.log(messages);
+    return messages;
   }
 
 }
